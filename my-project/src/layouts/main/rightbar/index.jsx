f315366c9718a2d6cac7bf0fcdb5a41de1800474
Nicat477 <nicatrzazad30@gmail.com>
2023-09-16 00:26:55 +0300

    first

---
+++ my-project/src/layouts/main/rightbar/index.jsx
@@ -0,0 +1,7 @@
+export default function Rightbar(){
+    return(
+        <aside className="w-[375px mr-2.5">
+            rightbar
+        </aside>
+    )
+}
---
+++ my-project/src/routes/index.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home";
+import Explore from "../pages/explore/inex";
+import Notifications from "../pages/notifications";
+import MainLayout from "../layouts/main";
+const routes=createBrowserRouter([
+    {
+        path:'/',
+        element:<MainLayout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>
+            },
+            {
+                path:'/explore',
+                element:<Explore/>
+            },
+            {
+                path:'/notifications',
+                element:<Notifications/>
+            }
+        ]
+    },
+   
+])
+export default routes
